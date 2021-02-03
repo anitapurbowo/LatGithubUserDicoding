99fbe5193427f998a971f6bb2ef6e42aefea0e2b
anitapurbowo <anitaforwork07@gmail.com>
2021-02-04 00:16:17 +0700

    ROOM

--- app/src/main/java/com/example/submission1/dataBase/dataBaseHelper.kt
+++ app/src/main/java/com/example/submission1/dataBase/dataBaseHelper.kt
@@ -1,4 +1,28 @@
 package com.example.submission1.dataBase
 
-internal class dataBaseHelper {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.submission1.userFav
+
+@Database(entities = arrayOf(userFav::class), version = 1)
+
+abstract class dataBaseHelper : RoomDatabase() {
+    abstract fun UserFunDAO() : userFavDAO
+    companion object {
+        private var INSTANCE : dataBaseHelper? = null
+        private val LOCK = Any()
+
+        operator fun invoke(context: Context) = INSTANCE ?: synchronized(LOCK) {
+            INSTANCE ?: buildDatabase(context).also {
+                INSTANCE = dataBaseHelper
+            }
+        }
+
+        private fun buildDatabase(context: Context) = Room.databaseBuilder(
+            context.applicationContext,
+            userFav::class.java,"dbUserFav.db"
+        ).build()
+    }
 }
